1f43a674fd415d4e76338c504069198a3ee8644e
avahak <a.vahakangas.work@gmail.com>
2023-10-25 17:06:29 +0300

    moved npm install directory to root

--- tba/__init__.py
+++ tba/__init__.py
@@ -1,3 +1,21 @@
-from flask import Flask
+# Initializing the Flask app in this __init__.py file ensures that it is set up
+# no matter which part of the package is accessed or executed. The Flask object is a
+# crucial component for all Flask-related functions within this package.
+#
+# Placing it here provides several advantages:
+#
+#  - It centralizes app initialization, ensuring a consistent configuration across
+#    multiple modules.
+#  - It makes the app configuration always available and accessible, no matter how
+#    the package is used.
+#  - It avoids tightly coupling the app with specific modules like views.py, making
+#    the code more modular and maintainable. Error handling or custom functionality
+#    might not use views, for example.
+#
+# In contrast, placing app initialization in the script used to execute the web application
+# (typically named app.py) could lead to issues, as it restricts the app's use only to web
+# hosting, limiting its utility for other tasks or custom functionality and hindering testing.
+
+from flask import Flask, render_template
 app = Flask("TBA", template_folder='tba/templates', static_folder='tba/static')
 # app = Flask("TBA") 
--- tba/views.py
+++ tba/views.py
@@ -1,4 +1,5 @@
 from flask import *
+import os
 from . import app       # finds variable app defined in __init__.py
 
 @app.route('/test')
@@ -13,6 +14,12 @@ def get_data():
 @app.route('/hello/<name>')
 def hello(name=None):
     return render_template("hello.html", name=name)
+
+@app.route('/node_modules/<path:filename>')
+def serve_node_modules(filename):
+    """Used by javascript imports.
+    """
+    return send_from_directory(app.root_path + "/node_modules", filename)
 
 @app.route('/')
 def home():
