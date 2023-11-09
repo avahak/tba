7e15345ab86866df5611a52b455bcf5cf5c46b83
avahak <a.vahakangas.work@gmail.com>
2023-11-09 05:04:30 +0200

    added nav_base.html

--- run.py
+++ run.py
@@ -1,8 +1,7 @@
 # Note: avoid having possibly conflicting names, such as app.py and folder app/, see
 # https://stackoverflow.com/questions/50157243/error-gunicorn-failed-to-find-application-object-app-in-app
 # gunicorn --bind=0.0.0.0 --timeout 600 run:app
-from app import create_app, db
-# from app.models import User, Role
+from app import create_app
 
 app = create_app()
 
